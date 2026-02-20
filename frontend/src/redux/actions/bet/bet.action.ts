8554dcfe65db38843f9f7af23aec972d92092bc8
lokeshvaishnav11 <lokeshkumariit@gmail.com>
2026-02-20 16:47:41 +0530

    VGHJ

--- frontend/src/redux/actions/bet/bet.action.ts
+++ frontend/src/redux/actions/bet/bet.action.ts
@@ -13,7 +13,9 @@ export const getPlaceBetAction = createAsyncThunk(
   async (data: IBet, { rejectWithValue, dispatch }) => {
     try {
       // Frontend delay of 2 seconds
+      if(data.betOn != "CASINO" && data.betOn != "MATKA"){
       await delay(1000)
+      }
 
       console.log(data, "check matka bet dtata")
       let res:any
