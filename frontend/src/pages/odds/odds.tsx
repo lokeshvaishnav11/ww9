998455a7b76f2e42fd209ebf16b379b342ed642d
lokesh Vaishnav <148864659+lokeshvaishnav11@users.noreply.github.com>
2026-05-20 22:11:21 +0530

    Update odds.tsx

--- frontend/src/pages/odds/odds.tsx
+++ frontend/src/pages/odds/odds.tsx
@@ -228,7 +228,7 @@ const Odds = () => {
         <iframe
           style={{ width: "100%", height: "auto" }}
           // src={`https://card.hr08bets.in/api/getScoreData?event_id=${currentMatch?.matchId}`}
-          src={`https://score.akamaized.uk/?id=${currentMatch?.matchId}`}
+         src={`https://scorecard.avrkhub.in/?v=2&btid=${currentMatch?.matchId}&etid=4`}  
         ></iframe>
       );
   };
